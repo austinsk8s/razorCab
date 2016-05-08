a19763cc7db21959946d1be6b25cfb8470d90967
Austin Williams <austinsk8s2day@gmail.com>
2016-05-08 20:24:20 +0000

    initial scafolding with user/role entities

---
+++ README.md
@@ -0,0 +1,4 @@
+razor_cab
+=========
+
+A Symfony project created on May 7, 2016, 7:11 pm.
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,211 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * User
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\UserRepository")
+ */
+class User
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+   /**
+    * @ORM\Column(type="string", length=25, unique=true)
+    */
+
+   private $username;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fname", type="string", length=255)
+     */
+    private $fname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lname", type="string", length=255)
+     */
+    private $lname;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Role")
+     *
+     */
+    private $role;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+  /**
+   * @inheritDoc
+   */
+  public function getUsername() {
+    return $this->username;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getSalt() {
+    return null;
+  }
+
+    /**
+     * Set fname
+     *
+     * @param string $fname
+     * @return User
+     */
+    public function setFname($fname)
+    {
+        $this->fname = $fname;
+
+        return $this;
+    }
+
+    /**
+     * Get fname
+     *
+     * @return string
+     */
+    public function getFname()
+    {
+        return $this->fname;
+    }
+
+    /**
+     * Set lname
+     *
+     * @param string $lname
+     * @return User
+     */
+    public function setLname($lname)
+    {
+        $this->lname = $lname;
+
+        return $this;
+    }
+
+    /**
+     * Get lname
+     *
+     * @return string
+     */
+    public function getLname()
+    {
+        return $this->lname;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     * @return User
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+  /**
+   * @see \Serializable::serialize()
+   */
+  public function serialize() {
+    return serialize(array(
+        $this->id,
+        $this->username,
+        $this->password,
+    ));
+  }
+
+  /**
+   * @see \Serializable::unserialize()
+   */
+  public function unserialize($serialized) {
+    list (
+            $this->id,
+            $this->username,
+            $this->password,
+            ) = unserialize($serialized);
+  }
+
+}
